d98f16b3814e3fa96561de37090b5dedf7929924
ReaganAdams <152213402+ReaganAdams@users.noreply.github.com>
2024-08-10 02:38:27 -0230

    Updated Ellipse.java

---
+++ Problem#2/Ellipse.java
@@ -0,0 +1,28 @@
+// Ellipse.java
+public class Ellipse extends Shape {
+    private double a; // major axis
+    private double b; // minor axis
+
+    public Ellipse(double axis1, double axis2) {
+        super("Ellipse");
+        this.a = Math.max(axis1, axis2);
+        this.b = Math.min(axis1, axis2);
+    }
+
+    @Override
+    public double computeArea() {
+        return Math.PI * a * b;
+    }
+
+    @Override
+    public double computePerimeter() {
+        return Math.PI * 2 * (a + b - Math.sqrt((a - b) * (a - b) / 2));
+    }
+
+    // New method to scale both axes by the given factor
+    @Override
+    public void scale(double factor) {
+        this.a *= factor;
+        this.b *= factor;
+    }
+}
