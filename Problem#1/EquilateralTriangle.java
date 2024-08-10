d4abeb097e5822f980e400473adf3953133ada35
ReaganAdams <152213402+ReaganAdams@users.noreply.github.com>
2024-08-10 02:25:41 -0230

    EquilateralTriangle.java

---
+++ Problem#1/EquilateralTriangle.java
@@ -0,0 +1,7 @@
+public class EquilateralTriangle extends Triangle {
+
+    public EquilateralTriangle(double side) {
+        super(side, side, side);
+        this.name = "EquilateralTriangle";
+    }
+}
