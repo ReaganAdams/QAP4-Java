69de50c414d16e8543fe3ea5bd41eaa0cfa96c06
ReaganAdams <152213402+ReaganAdams@users.noreply.github.com>
2024-08-10 02:26:32 -0230

    Demo.java

---
+++ Problem#1/Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[4];
+        
+        shapes[0] = new Circle(5.0);
+        shapes[1] = new Ellipse(6.0, 4.0);
+        shapes[2] = new Triangle(3.0, 4.0, 5.0);
+        shapes[3] = new EquilateralTriangle(5.0);
+
+        for (Shape shape : shapes) {
+            System.out.println(shape);
+        }
+    }
+}
