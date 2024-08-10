0065d007fee99631ace5cc4d4462a61ec4d37a67
ReaganAdams <152213402+ReaganAdams@users.noreply.github.com>
2024-08-10 02:37:35 -0230

    Updated Circle.java

---
+++ Problem#2/Circle.java
@@ -0,0 +1,25 @@
+// Circle.java
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        super("Circle");
+        this.radius = radius;
+    }
+
+    @Override
+    public double computeArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double computePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    // New method to scale the radius by the given factor
+    @Override
+    public void scale(double factor) {
+        this.radius *= factor;
+    }
+}
